10f2622477a7a30e125cafcd24faae1d02ff1008
ilyamakhon <ilyamakhon9135@gmail.com>
2020-02-19 21:56:59 +0300

    TourCriteriaNotFoundException added

---
+++ src/main/java/by/epam/traveltours/exception/TourCriteriaNotFoundException.java
@@ -0,0 +1,8 @@
+package by.epam.traveltours.exception;
+
+public class TourCriteriaNotFoundException extends RuntimeException {
+
+    public TourCriteriaNotFoundException(String message) {
+        super(message);
+    }
+}
